f658ef4e4227994a49ab8e6dade7adbfc79584d9
RaphaelFrey <raphael.marius.frey@gmail.com>
2024-03-21 20:06:28 +0100

    Added drag and drop

--- actions/copy-list/index.ts
+++ actions/copy-list/index.ts
@@ -58,6 +58,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                             title: card.title,
                             description: card.description,
                             order: card.order,
+                            boardId: card.boardId
                         })),
                     },
                 },
